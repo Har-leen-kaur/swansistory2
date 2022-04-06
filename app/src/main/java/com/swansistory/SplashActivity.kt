90c3dec8ee1d7611d60d4c671a2a2f9c491ec54a
haka03 <haka03@dfki.de>
2022-04-06 19:24:32 +0100

    splash screen working
    login introActivity not yet working

--- app/src/main/java/com/swansistory/SplashActivity.kt
+++ app/src/main/java/com/swansistory/SplashActivity.kt
@@ -1,11 +1,26 @@
 package com.swansistory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            // Start the Intro Activity
+            startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+            finish() // Call this when your activity is done and should be closed.
+        }, 2500)
     }
 }
