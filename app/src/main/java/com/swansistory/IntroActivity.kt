c6c5bf6408a30b6bc0bdc343acef95e807299f1b
haka03 <haka03@dfki.de>
2022-04-06 22:38:30 +0100

    add skip log in button and prettify beginning

--- app/src/main/java/com/swansistory/IntroActivity.kt
+++ app/src/main/java/com/swansistory/IntroActivity.kt
@@ -1,11 +1,20 @@
 package com.swansistory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        val skipSignInButton = findViewById<Button>(R.id.btn_skip_sign_in)
+
+        skipSignInButton.setOnClickListener{
+            val intent = Intent(this@IntroActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
